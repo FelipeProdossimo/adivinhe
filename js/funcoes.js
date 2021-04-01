f287485d2feeaf1e85e145fa7684236577d62f30
Felipe Prodossimo <felipe.profissional4@gmail.com>
2021-03-31 23:29:11 -0300

    Programa básico, de quem é esse pokemon, tente adivinhar!

---
+++ js/funcoes.js
@@ -0,0 +1,16 @@
+alert("Quem é esse pokemon?")
+
+let operacao = parseInt(prompt("Digite 1 para Pikachu, 2 para Charmander, 3 para Zubat e 4 para Bulbassauro"));
+
+if (operacao == 1) {
+    (alert("Parabéns, você tem uma ótima visão!!"));
+
+} else if (operacao == 2) {
+    (alert("Que pena, tente novamente!"));
+
+} else if (operacao == 3) {
+    (alert("Ops, parece que você errou, tente novamente!"));
+
+} else if (operacao == 4) {
+    (alert("Resultado errado, tente novamente!"));
+}
